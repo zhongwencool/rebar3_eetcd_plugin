4af2911ea92a8aec7bd1209f52b586f3c743398d
zhongwencool <zhongwencool@gmail.com>
2018-09-13 17:29:04 +0800

    update sample function

--- priv/eectd_sample_client.erl
+++ priv/eectd_sample_client.erl
@@ -8,15 +8,16 @@
 
 -module(eetcd_{{module_name}}).
 
+-include("router_pb.hrl").
 {{#methods}}
 -export([{{method}}/1]).
 {{/methods}}
 
 {{#methods}}
 %% @doc {{^output_stream}}{{^input_stream}}Unary RPC {{/input_stream}}{{#input_stream}}Stream RPC {{/input_stream}}{{/output_stream}}
--spec {{method}}({{pb_module}}:'{{input}}'()) ->
-    {{^output_stream}}{{^input_stream}}{ok, {{pb_module}}:'{{output}}'()}{{/input_stream}}{{#input_stream}}reference(){{/input_stream}}{{/output_stream}}{{#output_stream}}{{^input_stream}}reference(){{/input_stream}}{{#input_stream}}reference(){{/input_stream}}{{/output_stream}} | {error, term()}.
-{{method}}(Request) ->
+-spec {{method}}(#'{{input}}'{}) ->
+    {{^output_stream}}{{^input_stream}}{ok, #'{{output}}'{}}{{/input_stream}}{{#input_stream}}reference(){{/input_stream}}{{/output_stream}}{{#output_stream}}{{^input_stream}}reference(){{/input_stream}}{{#input_stream}}reference(){{/input_stream}}{{/output_stream}} | {error, {'grpc_error', non_neg_integer(), binary()}} | {error, term()}.
+{{method}}(Request) when is_record(Request, '{{input}}') ->
     {{^output_stream}}{{^input_stream}}eetcd_stream:unary(Request, <<"/etcdserverpb.{{unmodified_service_name}}/{{unmodified_method}}">>, '{{output}}'){{/input_stream}}{{#input_stream}}eetcd_stream:data(Request, <<"/etcdserverpb.{{unmodified_service_name}}/{{unmodified_method}}">>){{/input_stream}}{{/output_stream}}{{#output_stream}}{{^input_stream}}eetcd_stream:data(Request, <<"/etcdserverpb.{{unmodified_service_name}}/{{unmodified_method}}">>){{/input_stream}}{{/output_stream}}.
 
 {{/methods}}
